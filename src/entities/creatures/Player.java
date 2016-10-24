81bd30b7d877a136bdfee6a60ba72f23cfb84b76
turbek <turbek93@gmail.com>
2016-10-24 23:22:54 +0200

    a bit of refactor, but could be much better!

--- src/entities/creatures/Player.java
+++ src/entities/creatures/Player.java
@@ -41,39 +41,28 @@ public class Player extends Creature {
         }
         if (game.getKeyManager().left) {
             last = "left";
-            if (timer > 300000000) {
-                g.drawImage(Assets.antStayLeft, (int) x, (int) y, null);
-            } else {
-                g.drawImage(Assets.antRunLeft, (int) x, (int) y, null);
-            }
+            if (timer > 300000000) { g.drawImage(Assets.antStayLeft, (int) x, (int) y, null); }
+            else { g.drawImage(Assets.antRunLeft, (int) x, (int) y, null); }
         }
         else if (game.getKeyManager().right) {
             last = "right";
-            if (timer > 300000000) {
-                g.drawImage(Assets.antStayRight, (int) x, (int) y, null);
-            } else {
-                g.drawImage(Assets.antRunRight, (int) x, (int) y, null);
-            }
+            if (timer > 300000000) { g.drawImage(Assets.antStayRight, (int) x, (int) y, null); }
+            else { g.drawImage(Assets.antRunRight, (int) x, (int) y, null); }
         }
         else if (game.getKeyManager().up) {
             last = "up";
-            if (timer > 300000000) {
-                g.drawImage(Assets.antStayUp, (int) x, (int) y, null);
-            } else {
-                g.drawImage(Assets.antRunUp, (int) x, (int) y, null);
-            }
+            if (timer > 300000000) { g.drawImage(Assets.antStayUp, (int) x, (int) y, null); }
+            else { g.drawImage(Assets.antRunUp, (int) x, (int) y, null); }
         }
         else if (game.getKeyManager().down) {
             last = "down";
-            if (timer > 300000000) {
-                g.drawImage(Assets.antStayDown, (int) x, (int) y, null);
-            } else {
-                g.drawImage(Assets.antRunDown, (int) x, (int) y, null);
-            }
-        } else if (last.equals("left")){ g.drawImage(Assets.antStayLeft, (int) x, (int) y, null);}
-        else if (last.equals("right")) { g.drawImage(Assets.antStayRight, (int) x, (int) y, null);}
-        else if (last.equals("up")) { g.drawImage(Assets.antStayUp, (int) x, (int) y, null);}
-        else if (last.equals("down")) { g.drawImage(Assets.antStayDown, (int) x, (int) y, null);}
+            if (timer > 300000000) { g.drawImage(Assets.antStayDown, (int) x, (int) y, null); }
+            else { g.drawImage(Assets.antRunDown, (int) x, (int) y, null); }
+
+        } else if (last.equals("left")){ g.drawImage(Assets.antStayLeft, (int) x, (int) y, null); }
+        else if (last.equals("right")) { g.drawImage(Assets.antStayRight, (int) x, (int) y, null); }
+        else if (last.equals("up")) { g.drawImage(Assets.antStayUp, (int) x, (int) y, null); }
+        else if (last.equals("down")) { g.drawImage(Assets.antStayDown, (int) x, (int) y, null); }
         else { g.drawImage(Assets.antStayLeft, (int) x, (int) y, null); }
 
         }
