f37b29d71d1435613c3dbd95e5c631b2ee7a3f8b
handriss <andrashinkel@gmail.com>
2016-10-25 18:42:55 +0200

    a

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,7 +6,7 @@ import utils.Utils;
 public class Main {
     public static void main(String[] args){
 
-        Utils.playMusic();
+//        Utils.playMusic();
         Game game = new Game("Niche", 620, 682);
         game.start();
     }
